9e83bd9d6f74fa3c7c4722ef4a4059921b0d7aa3
akylove <akyama.vieira@rr.sebrae.com.br>
2024-10-14 16:42:53 -0400

    fix: Resolução de dependências

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['C:/Users/rodrigo.lacan/OneDrive - SEBRAE/PESSOAL/GitHub/Serviços Sebrae 2.0.0/Servicos_Sebrae_testing-/resources/css/Template/main.css', 'resources/js/app.js'],
+            input: ['resources/css/Template/main.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
