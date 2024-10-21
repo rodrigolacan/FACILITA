ad01e0f0e33c8af5e97554830ace9a2a4c83f1e5
akylove <akyama.vieira@rr.sebrae.com.br>
2024-10-21 16:08:06 -0400

    feat: Mudança de cor e fontes na tela de login

--- resources/views/Login/Login_Forms.blade.php
+++ resources/views/Login/Login_Forms.blade.php
@@ -9,28 +9,26 @@
     @vite(['resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-to-bl from-[#005EB8] to-[#000000] h-screen flex flex-col items-center justify-center">
+<body class="bg-gradient-to-bl from-[#3B4AFF] to-[#9285F9] h-screen flex flex-col items-center justify-center">
     <!-- Texto de Boas-vindas -->
     <div class="flex items-center justify-between w-full px-[150px]">
         <div class="w-1/2 flex flex-col justify-center">
             <!-- Título Facilita -->
             <h1
-                class="{{ env('APP_ENV') === 'production' ? 'text-[#00BFFF]' : (env('APP_ENV') === 'homolog' ? 'text-red-500' : 'text-white') }} font-fugaz text-[80px]">
+                class="{{ env('APP_ENV') === 'production' ? 'text-white' : (env('APP_ENV') === 'homolog' ? 'text-[#FFA726]' : 'text-white') }} font-fugaz text-[80px]">
                 Facilita
             </h1>
             <!-- Parágrafo de introdução -->
-            <p class="text-white font-['Roboto'] font-semibold text-[50px] leading-tight mt-4">
+            <p class="text-white font-['Poppins'] font-semibold text-[50px] leading-tight mt-4">
                 Bem-vindo ao Facilita! Sua intranet inteligente para otimizar tarefas e conectar ideias.
             </p>
             <!-- Subtexto -->
-            <p class="text-white font-roboto text-[24px] opacity-40 mt-4">
+            <p class="text-white font-poppins text-[24px] opacity-40 mt-4">
                 Faça login e comece a explorar!
             </p>
         </div>
@@ -41,7 +39,7 @@
                 @csrf
                 <div class="w-full max-w-md space-y-4">
                     <!-- Input de Nome de Usuário -->
-                    <div class="flex items-center border-b-2 border-[#12325d] py-2">
+                    <div class="flex items-center border-b-2 border-[#3B4AFF] py-2">
                         <span class="material-icons text-white pr-2">
                             person
                         </span>
@@ -51,7 +49,7 @@
                     </div>
 
                     <!-- Input de Senha -->
-                    <div class="flex items-center border-b-2 border-[#12325d] py-2">
+                    <div class="flex items-center border-b-2 border-[#3B4AFF] py-2">
                         <span class="material-icons text-white pr-2">
                             lock
                         </span>
@@ -62,7 +60,7 @@
                 </div>
                 <div class="text-center">
                     <button type="submit" class="w-full py-2 bg-white rounded-[32px] flex justify-center items-center">
-                        <span class="text-[#00224f] text-2xl font-bold font-roboto">Login</span>
+                        <span class="text-[#00224f] text-2xl font-bold font-poppins">Login</span>
                     </button>
                 </div>
 
